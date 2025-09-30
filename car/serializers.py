7117c9c79c16706f7c510143ec8ab55698478b2e
Ihor <andreiunderwhat@mail.com>
2025-09-30 22:00:42 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -7,7 +7,7 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField()
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(required=False)
 
@@ -18,7 +18,7 @@ class CarSerializer(serializers.Serializer):
         instance.manufacturer = validated_data.get("manufacturer",
                                                    instance.manufacturer)
         instance.model = validated_data.get("model", instance.model)
-        instance.horse_power = validated_data.get("horse_powers",
+        instance.horse_powers = validated_data.get("horse_powers",
                                                   instance.horse_powers)
         instance.is_broken = validated_data.get("is_broken",
                                                 instance.is_broken)
--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def serialize_car_object(car: Car) -> bytes:
 def deserialize_car_object(json: bytes) -> Car:
     stream = io.BytesIO(json)
     data = JSONParser().parse(stream)
-    serializer = CarSerializer(instance=data)
-    return serializer.validated_data
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
