9ac21c2780565371d3cef1609b5cec54aeeff8f7
Ihor <andreiunderwhat@mail.com>
2025-09-30 22:04:32 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -19,7 +19,7 @@ class CarSerializer(serializers.Serializer):
                                                    instance.manufacturer)
         instance.model = validated_data.get("model", instance.model)
         instance.horse_powers = validated_data.get("horse_powers",
-                                                  instance.horse_powers)
+                                                   instance.horse_powers)
         instance.is_broken = validated_data.get("is_broken",
                                                 instance.is_broken)
         instance.problem_description = validated_data.get(
